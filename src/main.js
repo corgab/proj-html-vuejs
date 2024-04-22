ab92cd71739aa09b22e525f9473651f295d60af6
gab <corbanigabriele@gmail.com>
2024-04-22 14:09:43 +0200

    ADD: Font awesome installato ed importato globalmente

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import icons
+import {
+	faStar,
+	faCartShopping
+	// faPlay,
+	// faUser,
+} from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faStar, faCartShopping)
+
+import App from './App.vue'
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
+

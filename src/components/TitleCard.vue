9b951fce23254edab0e0c22f188589d629cd688e
gab <corbanigabriele@gmail.com>
2024-04-24 14:53:25 +0200

    ADD: aggiunte le stelle

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -13,13 +13,20 @@
             <a href="#" class="btn btn-outline-light my-2">EXPLORE THE MENU</a>
         </div>
     </div> -->
-    <div class="d-flex card-width gap-5 p">
+    <div class="d-flex card-width gap-5 p ">
         <div class="trattino">
             <font-awesome-icon class="text--gray icon" :icon="['fas', 'minus']" />
         </div>
         <div class="content">
             <p class="text--gray lh-base"> {{ titolino }}</p>
             <h1 class="title text-white lh-base fw-bold"> {{ title }}</h1>
+            <ul class="d-flex">
+                <li v-for="star in 5" class="text--gray star">
+                    <!-- <font-awesome-icon class="text--gray icon" :icon="['fas', 'star']" /> -->
+                    {{ stars }}
+                </li>
+                
+            </ul>
             <p class="text--gray lh-lg"> {{ desc }}
             </p>
             <a href="#" class="btn btn-outline-light my-4"> {{ btn }}</a>
@@ -34,6 +41,7 @@
             titolino: String,
             desc: String,
             btn: String,
+            stars: String
         }
     }
 </script>
@@ -41,10 +49,6 @@
 <style lang="scss" scoped>
 .card-width {
     width: 400px;
-}
-
-.icon {
-    font-size: 20px;
 }
 
 .p {
@@ -57,5 +61,9 @@
 
 .title {
     font-size: 3rem;
+}
+
+.star {
+    padding: 10px;
 }
 </style>

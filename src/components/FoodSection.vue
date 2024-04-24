152fc205add172b6891f374e5577d14ddb5b05f1
gab <corbanigabriele@gmail.com>
2024-04-24 15:55:12 +0200

    MODIFY: gestita img come bg per renderla meno stretchata

--- src/components/FoodSection.vue
+++ src/components/FoodSection.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="section">
-        <img src="../assets/images/menu272x.jpg" alt="">
+    <div class="section bg">
+        <!-- <img src="../assets/images/menu272x.jpg" alt=""> -->
         <h3 class="text--gray">KUNG PAO CHICKEN - $32</h3>
     </div>
 </template>
@@ -14,9 +14,14 @@
 <style lang="scss" scoped>
 .section {
     position: relative;
+    height: 500px;
 }
-img {
-    height: 690px;
+
+.bg {
+    background-image: url(../assets/images/menu272x.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
 }
 
 h3 {

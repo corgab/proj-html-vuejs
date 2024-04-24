cfce3ab782ac4e9fde60fe714fa55550e1529be4
gab <corbanigabriele@gmail.com>
2024-04-24 15:38:44 +0200

    ADD: Feedback completo

--- src/components/FeedbackSection.vue
+++ src/components/FeedbackSection.vue
@@ -4,16 +4,32 @@
             <h1>What critics are saying about us</h1>
             <font-awesome-icon class="text--gray icon" :icon="['fas', 'minus']" />
         </div>
-        <div class="row">
+        <div class="row g-0">
             <div class="col-6">
                 <img src="../assets/images/blog72x-400x458.jpg" alt="">
             </div>
             <div class="col-6 bg-black cart">
-                <TitleCard titolino="MEGGY STEWART" stars="&star;" title="NEW YORK TIMES" desc="Non arcu mauris tortor ultrices mollis tellus euismod fermentum.
-                 Habitant amet tincidunt id sapien accumsan sed at.
-                 Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi." btn="READ FULL ARTICLE" />
-
+                <div class="px-3">
+                    <TitleCard titolino="MEGGY STEWART" stars="&starf;" title="NEW YORK TIMES" desc="Non arcu mauris tortor ultrices mollis tellus euismod fermentum.
+                        Habitant amet tincidunt id sapien accumsan sed at.
+                        Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi." btn="READ FULL ARTICLE" />
+                </div>
             </div>
+        </div>
+        <div class="my-5 d-flex justify-content-between">
+            <div class="bg-mary">
+                <div class="overlay p-4">
+                    <TitleCard titolino="MARY MAXEY" title="THE GUARDIAN" stars="&starf;" desc="Non arcu mauris tortor ultrices mollis tellus euismod fermentum.
+                        Habitant amet tincidunt id sapien accumsan sed." btn="READ FULL ARTICLE" />
+                </div>
+            </div>
+            <div class="bg-patrik">
+                <div class="overlay p-4">
+                    <TitleCard titolino="MARY MAXEY" title="THE GUARDIAN" stars="&starf;" desc="Non arcu mauris tortor ultrices mollis tellus euismod fermentum.
+                Habitant amet tincidunt id sapien accumsan sed." btn="READ FULL ARTICLE" />
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -42,5 +58,24 @@ img {
 
 .cart:hover {
     transform: scale(1.1);
+}
+
+.overlay {
+    background: rgb(0, 0, 0);
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0.9) 0%, rgba(255, 255, 255, 0) 100%);
+}
+
+.bg-mary {
+    background-image: url(../assets/images/info52x.jpg);
+}
+
+.bg-patrik {
+    background-image: url(../assets/images/info12x.jpg);
+}
+
+.bg-mary,
+.bg-patrik {
+    background-size: contain;
+    background-repeat: no-repeat;
 }
 </style>
--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -1,18 +1,4 @@
 <template>
-    <!-- <div class="d-flex card-width gap-5 p">
-        <div class="trattino">
-            <font-awesome-icon class="text--gray icon" :icon="['fas', 'minus']" />
-        </div>
-        <div class="content">
-            <p class="text--gray lh-base">THE BEST TABLE IN TOWN</p>
-            <h1 class="title text-white lh-base">FINE DINING EXPERIENCE</h1>
-            <p class="text--gray lh-base">Pellentesque vitae viverra risus, sagittis.
-                Venenatis ridiculus scelerisque nisi in urna nulla.
-                Sit tempor a et nisl, ac felis.
-            </p>
-            <a href="#" class="btn btn-outline-light my-2">EXPLORE THE MENU</a>
-        </div>
-    </div> -->
     <div class="d-flex card-width gap-5 p ">
         <div class="trattino">
             <font-awesome-icon class="text--gray icon" :icon="['fas', 'minus']" />
@@ -25,8 +11,8 @@
                     <!-- <font-awesome-icon class="text--gray icon" :icon="['fas', 'star']" /> -->
                     {{ stars }}
                 </li>
-                
             </ul>
+            <!-- <font-awesome-icon class="text--gray icon" :icon="['fas', 'minus']" /> -->
             <p class="text--gray lh-lg"> {{ desc }}
             </p>
             <a href="#" class="btn btn-outline-light my-4"> {{ btn }}</a>
